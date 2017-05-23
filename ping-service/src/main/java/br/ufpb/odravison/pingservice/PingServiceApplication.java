d2cc0a301bff3eaa80db9c349fb5589e3bd8b7c0
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-05-23 00:41:24 -0300

    fix

--- ping-service/src/main/java/br/ufpb/odravison/pingservice/PingServiceApplication.java
+++ ping-service/src/main/java/br/ufpb/odravison/pingservice/PingServiceApplication.java
@@ -3,9 +3,11 @@ package br.ufpb.odravison.pingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PingServiceApplication {
 
 	public static void main(String[] args) {
